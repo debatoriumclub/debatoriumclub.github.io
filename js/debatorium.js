3a15d2dd3455fc999e8d05bb9b2bf2a0a9446819
Oleksandr Kyetov <oleksandr.kyetov@gmail.com>
2016-05-08 20:34:36 +0300

    Maria's photo changed. Draft version of the calendar added

---
+++ js/debatorium.js
@@ -0,0 +1,32 @@
+var init = {
+	map: function() {
+		var map = new google.maps.Map(document.querySelector('.location__map'), {
+			center: {
+				lat: 50.4471435,
+				lng: 30.5160441
+			},
+			zoom: 17,
+			zoomControl: true,
+			panControl: false,
+			scrollwheel: false,
+			rotateControl: false,
+			scaleControl: false,
+			mapTypeControl: false,
+			streetViewControl: false,
+			overviewMapControl: false
+		});
+
+		var marker = new google.maps.Marker({
+			position: {
+				lat: 50.4474271,
+				lng: 30.5159097
+			},
+			map: map
+		});
+	}
+};
+
+$('.datepicker').pickadate({
+	selectMonths: true,
+	selectYears: 15
+});
