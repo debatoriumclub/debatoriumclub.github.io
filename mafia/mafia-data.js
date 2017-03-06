3e999103b05ebccd7653e2342f033b56cbdb3f0b
Oleksandr Kyetov <oleksandr.kyetov@gmail.com>
2017-03-06 20:34:45 +0200

    Added book talks trusted members

---
+++ mafia/mafia-data.js
@@ -0,0 +1,210 @@
+var data = [
+	{
+		date: {
+			year: 2016,
+			month: 'July',
+			day: 22
+		},
+		winner: 'City',
+		players: [
+			{
+				name: 'June',
+				role: 'Citizen'
+			},
+			{
+				name: 'Scott',
+				role: 'Mafia'
+			},
+			{
+				name: 'Bob',
+				role: 'Citizen'
+			},
+			{
+				name: 'Monica',
+				role: 'Citizen'
+			},
+			{
+				name: 'Sunny Bunny',
+				role: 'Citizen'
+			},
+			{
+				name: 'Bonnie',
+				role: 'Detective'
+			},
+			{
+				name: 'Archie Goodwin',
+				role: 'Don'
+			},
+			{
+				name: 'Ginny',
+				role: 'Mafia'
+			},
+			{
+				name: 'The Godfather',
+				role: 'Citizen'
+			},
+			{
+				name: 'Rosalia',
+				role: 'Citizen'
+			},
+		],
+		moves: [
+			{
+				night: 9,
+				day: undefined
+			},
+			{
+				night: 3,
+				day: 7
+			},
+			{
+				night: 6,
+				day: undefined
+			},
+			{
+				night: 6,
+				day: undefined
+			},
+			{
+				night: undefined,
+				day: 2
+			},
+			{
+				night: 10,
+				day: 8
+			}
+		]
+	},
+	{
+		date: {
+			year: 2016,
+			month: 'July',
+			day: 22
+		},
+		winner: 'City',
+		players: [
+			{
+				name: 'Archie Goodwin',
+				role: 'Detective'
+			},
+			{
+				name: 'The Godfather',
+				role: 'Mafia'
+			},
+			{
+				name: 'Bonnie',
+				role: 'Citizen'
+			},
+			{
+				name: 'Bob',
+				role: 'Citizen'
+			},
+			{
+				name: 'Raiden',
+				role: 'Citizen'
+			},
+			{
+				name: 'Monica',
+				role: 'Citizen'
+			},
+			{
+				name: 'Sunny Bunny',
+				role: 'Don'
+			},
+			{
+				name: 'Ginny',
+				role: 'Citizen'
+			},
+			{
+				name: 'Chili',
+				role: 'Citizen'
+			},
+			{
+				name: 'Rosalia',
+				role: 'Mafia'
+			},
+		],
+		moves: [
+			{
+				night: 1,
+				day: 2
+			},
+			{
+				night: 3,
+				day: 5
+			},
+			{
+				night: 9,
+				day: 10
+			},
+			{
+				night: 8,
+				day: 7
+			}
+		]
+	},
+	{
+		date: {
+			year: 2016,
+			month: 'July',
+			day: 22
+		},
+		winner: 'Mafia',
+		players: [
+			{
+				name: 'Archie Goodwin',
+				role: 'Don'
+			},
+			{
+				name: 'Ginny',
+				role: 'Citizen'
+			},
+			{
+				name: 'Raiden',
+				role: 'Citizen'
+			},
+			{
+				name: 'Rachel',
+				role: 'Mafia'
+			},
+			{
+				name: 'June',
+				role: 'Citizen'
+			},
+			{
+				name: 'Chili',
+				role: 'Citizen'
+			},
+			{
+				name: 'Lucia',
+				role: 'Mafia'
+			},
+			{
+				name: 'The Godfather',
+				role: 'Citizen'
+			},
+			{
+				name: 'Katron',
+				role: 'Detective'
+			},
+			{
+				name: 'Rosalita',
+				role: 'Citizen'
+			},
+		],
+		moves: [
+			{
+				night: 2,
+				day: 7
+			},
+			{
+				night: 3,
+				day: 9
+			},
+			{
+				night: 5,
+				day: 8
+			}
+		]
+	}
+]
